028935266a8910d285bf636217c9cdcf5941255a
farrux <[200~https://github.com/farruxerimmatov-crypto~>
2025-12-26 23:22:13 +0500

    i

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,14 +1,17 @@
 const {Router} = require("express")
 const { getallAuthors, addAuthors, getoneAuthor, updateAuthor, deleteAuthor, search } = require("../controller/auth.controller")
+const authorValidationMiddleware = require("../middleware/author-validation.middleware")
+const auhtorization = require("../middleware/auhtorization")
+
 
 
 const authorRoutes = Router()
 
 authorRoutes.get("/get_all_authors", getallAuthors)
 authorRoutes.get("/search", search)
-authorRoutes.post("/add_auhtors", addAuthors)
-authorRoutes.get("/get_one_author/:id", getoneAuthor)
-authorRoutes.put("/update_author/:id", updateAuthor)
-authorRoutes.delete("/delete_author/:id",  deleteAuthor)
+authorRoutes.post("/add_auhtors",   authorValidationMiddleware, auhtorization, addAuthors)
+authorRoutes.get("/get_one_author/:id", auhtorization , getoneAuthor)
+authorRoutes.put("/update_author/:id",  auhtorization , updateAuthor)
+authorRoutes.delete("/delete_author/:id",  auhtorization ,  deleteAuthor)
 
 module.exports = authorRoutes 
